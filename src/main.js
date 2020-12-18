00fd1d07b76364a9bb433e3375d1cc24de08f063
tsuchiyaisshin <tuchiyaisshin@icloud.com>
2020-12-18 23:32:48 +0900

    storeの削除

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/global.css'
-import store from './store/index'
 import VueApexCharts from 'vue3-apexcharts'
 
 const myApp = createApp(App)
